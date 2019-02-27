92955e7af1cce41de2dc5ac7eb9384f14d822843
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2019-02-27 12:04:08 +0100

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,3 +1,3 @@
 <?php
-    $bdd = new PDO('mysql:host=mysql-ivax.alwaysdata.net;dbname=ivax_login', 'ivax', 'XAluhu2620!');
+    $bdd = new PDO('mysql:host=mysql-ivax.alwaysdata.net;dbname=ivax_login', 'ivax', 'sdhksghkds');
 ?>
